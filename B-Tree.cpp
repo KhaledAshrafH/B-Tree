e7ac56602d901d5c576568003bf45698758b5d09
Khaled Ashraf <83093446+KhaledAshrafH@users.noreply.github.com>
2024-12-11 05:02:22 +0200

    Refactoring code and fix bugs
    Refactoring code and fix bugs in new versions of compilers

--- B-Tree.cpp
+++ B-Tree.cpp
@@ -6,17 +6,17 @@ template<class T,int Order>
 struct Node
 {
     //Node *Parent;
-    int NumbersOfKeys;//number of the actual keys
+    int number_of_keys;//number of the actual keys
     int order;
     int position=-1;//to allocate value in the appropriate place
-    T* keys=new T[order];
-    Node** childs=new Node*[order];
+    T* keys;
+    Node** children;
 
-    Node (int order);
+    explicit Node (int order);
     int Insert (T value);
-    Node* split (Node* node, T* value);
+    Node* split (Node* node, T* med);
     void Print () ;
-    void PrintUtil (int height,bool checkParent);
+    void PrintUtil (int height,bool checkRoot);
     int getHeight () ;
     ~Node ();
 };
@@ -27,90 +27,94 @@ template <class T,int Order>
 Node<T,Order>::Node (int order)
 {
     this->order = order;
-    this->NumbersOfKeys = 0;
-}
+    this->number_of_keys = 0;
+    // Allocate memory AFTER setting order
+    this->keys = new T[this->order];
+    this->children = new Node*[this->order + 1];
 
+    // Initialize children to NULL
+    for (int i = 0; i <= this->order; ++i)
+        this->children[i] = nullptr;
+}
 
 
 template <class T,int Order>
 int Node<T,Order>::Insert (T value)
 {
 
-    //if the node is leaf
-    if (this->childs[0] == NULL)
+    //if the node is leafed
+    if (this->children[0] == nullptr)
     {
         this->keys[++this->position] = value;
-        ++this->NumbersOfKeys;
-        //arrange the keys array after put new value in node
+        ++this->number_of_keys;
+        //arrange the key array after put new value in node
         for(int i=this->position; i>0 ; i--)
-        {
-            if (this->keys[i] < this->keys[i-1]) std::swap(this->keys[i],this->keys[i-1]);
-        }
+            if (this->keys[i] < this->keys[i-1])
+                std::swap(this->keys[i],this->keys[i-1]);
     }
     //if the node is not leaf
     else
     {
         //count to get place of child to put the value in it
         int i=0;
-        for(; i<this->NumbersOfKeys>0 && value > this->keys[i];)
-        {
+        for(; i<this->number_of_keys && value > this->keys[i];)
             i++;
-        }
+
         //Check if the child is full to split it
-        int check=this->childs[i]->Insert(value);
+        int check=this->children[i]->Insert(value);
         //if node full
         if(check)
         {
             T mid;
             int TEMP = i;
-            Node<T,Order> *newNode = split(this->childs[i], &mid); //Splitted Node to store the values and child that greater than the midValue
-            //allocate midValue in correct place
-            for(; i<this->NumbersOfKeys>0 && mid > this->keys[i];)
-            {
+            Node<T,Order> *newNode = split(this->children[i], &mid); //Split Node
+            // to store the values and child that greater than the midValue
+            //allocate midValue the correct place
+            for(; i<this->number_of_keys && mid > this->keys[i];)
                 i++;
-            }
 
-            for (int j = this->NumbersOfKeys; j > i ; j--) this->keys[j] = this->keys[j - 1];
+            for (int j = this->number_of_keys; j > i ; j--)
+                this->keys[j] = this->keys[j - 1];
             this->keys[i] = mid;
-
-            ++this->NumbersOfKeys;
+            ++this->number_of_keys;
             ++this->position;
 
-            //allocate newNode Splitted in the correct place
+            //allocate newNode Split in the correct place
             int k;
-            for (k = this->NumbersOfKeys; k > TEMP + 1; k--)
-                this->childs[k] = this->childs[k - 1];
-            this->childs[k] = newNode;
+            for (k = this->number_of_keys; k > TEMP + 1; k--)
+                this->children[k] = this->children[k - 1];
+            this->children[k] = newNode;
         }
 
     }
-    if(this->NumbersOfKeys == this->order) return 1;//to split it
+    if(this->number_of_keys == this->order)
+        return 1;//to split it
     else return 0;
 }
 
 template <class T,int Order>
 Node<T,Order>* Node<T,Order>::split (Node *node, T *med) //mid to store value of mid and use it in insert func
 {
-    int NumberOfKeys = node->NumbersOfKeys;
-    Node<T,Order> *newNode = new Node<T,Order>(order);
+    int NumberOfKeys = node->number_of_keys;
+    auto *newNode = new Node<T,Order>(order);
     //Node<T,Order> *newParentNode = new Node<T,Order>(order);
     int midValue = NumberOfKeys / 2;
     *med = node->keys[midValue];
     int i;
-    //take the values after mid value
+    //take the values after mid-value
     for (i = midValue + 1; i < NumberOfKeys; ++i)
     {
         newNode->keys[++newNode->position] = node->keys[i];
-        newNode->childs[newNode->position] = node->childs[i];
-        ++newNode->NumbersOfKeys;
+        newNode->children[newNode->position] = node->children[i];
+        ++newNode->number_of_keys;
         --node->position;
-        --node->NumbersOfKeys;
-        node->childs[i] = NULL;
+        --node->number_of_keys;
+        node->children[i] = nullptr;
     }
-    newNode->childs[newNode->position+1] = node->childs[i];
-    node->childs[i] = NULL;
+    newNode->children[newNode->position + 1] = node->children[i];
+    node->children[i] = nullptr;
 
-    --node->NumbersOfKeys; //because we take mid value...
+    --node->number_of_keys; //because we take mid-value...
     --node->position;
     return newNode;
 }
@@ -136,22 +140,20 @@ void Node<T,Order>::PrintUtil (int height,bool checkRoot)
     //to print all values in the level
     if (height==1 || checkRoot)
     {
-        for (int i = 0; i < this->NumbersOfKeys; i++){
+        for (int i = 0; i < this->number_of_keys; i++){
              if(i==0) cout << "|";
              cout<< this->keys[i];
-             if(i!=this->NumbersOfKeys-1) cout<<"|";
-             if(i==this->NumbersOfKeys-1) cout << "|"<<" ";
+             if(i!=this->number_of_keys-1) cout<<"|";
+             if(i==this->number_of_keys-1) cout << "|"<<" ";
         }
 
     }
 
     else
     {
-        for (int i = 0; i <= this->NumbersOfKeys; i++){
-            this->childs[i]->PrintUtil(height-1,false);
+        for (int i = 0; i <= this->number_of_keys; i++)
+            this->children[i]->PrintUtil(height - 1, false);
             //cout<<endl<<" ";
-        }
-
     }
 
 }
@@ -162,22 +164,23 @@ int Node<T,Order>::getHeight ()
     int COUNT=1;
     Node<T,Order>* Current=this;//current point to root
     while(true){
-            //is leaf
-        if(Current->childs[0] == NULL){
+            //is leafed
+        if(Current->children[0] == nullptr)
             return COUNT;
-        }
-        Current=Current->childs[0];
+        Current=Current->children[0];
         COUNT++;
     }
 }
 
-//Deallocation
+// De-allocation
 template <class T,int Order>
 Node<T,Order>::~Node ()
 {
     delete[]keys;
-    for (int i = 0; i <= this->NumbersOfKeys; ++i)
-        delete this->childs[i];
+    // Only delete non-NULL child pointers
+    for (int i = 0; i <= this->number_of_keys; ++i)
+        delete children[i];
+    delete[] children;
 }
 /////////////////////////////////////////////////////////////
 
@@ -187,9 +190,9 @@ template <class T,int Order>
 class BTree
 {
 private:
-    Node<T,Order> *Root;
+    Node<T,Order> *root;
     int order;
-    int count=0;//to count number of elements
+    int count=0; //to count the number of elements
 
 public:
     BTree ();
@@ -205,33 +208,34 @@ template <class T,int Order>
 BTree<T,Order>::BTree()
 {
     this->order = Order;
-    this->Root  = NULL;
+    this->root  = nullptr;
+    this->count=0;
 }
 
 template <class T,int Order>
 void BTree<T,Order>::Insert (T value)
 {
     count++;
-    //if Tree is empty
-    if (this->Root == NULL)
+    //if the Tree is empty
+    if (this->root == nullptr)
     {
-        this->Root = new Node<T,Order>(this->order);
-        this->Root->keys[++this->Root->position]=value;
-        this->Root->NumbersOfKeys=1;
+        this->root = new Node<T,Order>(this->order);
+        this->root->keys[++this->root->position]=value;
+        this->root->number_of_keys=1;
     }
-    //if tree not empty
+    //if a tree doesn't empty
     else
     {
-        int check=Root->Insert(value);
+        int check=root->Insert(value);
         if(check){
             T mid;
-            Node<T,Order> *splittedNode = this->Root->split(this->Root, &mid);
-            Node<T,Order> *newNode = new Node<T,Order>(this->order);
+            Node<T,Order> *splitNode = this->root->split(this->root, &mid);
+            auto *newNode = new Node<T,Order>(this->order);
             newNode->keys[++newNode->position]=mid;
-            newNode->NumbersOfKeys=1;
-            newNode->childs[0] = Root;
-            newNode->childs[1] = splittedNode;
-            this->Root = newNode;
+            newNode->number_of_keys=1;
+            newNode->children[0] = root;
+            newNode->children[1] = splitNode;
+            this->root = newNode;
         }
     }
 }
@@ -240,8 +244,8 @@ void BTree<T,Order>::Insert (T value)
 template <class T,int Order>
 void BTree<T,Order>::Print () const
 {
-    if (Root != NULL)
-        Root->Print();
+    if (root != nullptr)
+        root->Print();
     else cout<<"The B-Tree is Empty"<<endl;
 }
 
@@ -249,14 +253,13 @@ void BTree<T,Order>::Print () const
 template <class T,int Order>
 BTree<T,Order>::~BTree ()
 {
-    delete Root;
+    delete root;
 }
 
 
 ////////////////////////////////////////////////////////////
 int main ()
 {
-
     // Construct a BTree of order 3, which stores int data
     BTree<int,3> t1;
     t1.Insert(1);
